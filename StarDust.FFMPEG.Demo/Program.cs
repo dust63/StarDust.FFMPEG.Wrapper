a21cefd76e8bef3d879f0cdfdde37e144e773659
dust63 <dust63@hotmail.fr>
2020-02-01 00:01:35 +0100

    Add project files.

---
+++ StarDust.FFMPEG.Demo/Program.cs
@@ -0,0 +1,29 @@
+﻿using StarDust.FFMPEG.Wrapper;
+using System;
+using System.Threading.Tasks;
+
+namespace StarDust.FFMPEG.Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            Console.WriteLine("Lauching ffmpeg");
+            var ffmpegPath = @"C:\ProgramData\chocolatey\lib\ffmpeg\tools\ffmpeg\bin\ffmpeg.exe";
+            var wrapper = new FFProcessWrapper(ffmpegPath);
+            wrapper.ErrorDataReceived += (s, e) => Console.Error.WriteLine(e.Data);
+            wrapper.OutputDataReceived += (s, e) => Console.WriteLine(e.Data);
+            Task.Run(async ()=> await wrapper.Start("-f gdigrab -framerate 30 -i desktop -y D:\\output.mkv"));
+            Console.WriteLine($"FFmpeg is ruuning on process: {wrapper?.FFMpegProcess?.Id}");
+            Console.ReadKey();
+            wrapper.Stop().GetAwaiter().GetResult();
+            Console.WriteLine("ffmpeg stopped");
+            Console.WriteLine("Tap any key to exit...");
+            Console.ReadKey();
+            Console.WriteLine("End of the demo");          
+        }
+    }
+}
---
+++ StarDust.FFMPEG.Wrapper/FFProcessWrapper.cs
@@ -0,0 +1,173 @@
+﻿using DeferredEvents;
+using StarDust.FFMPEG.Wrapper.Helpers;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace StarDust.FFMPEG.Wrapper
+{
+    public class FFProcessWrapper
+    {
+        public EventHandler<FFMpegOutputEventArgs> OutputDataReceived;
+
+        public delegate void OutputDataReceivedEventHandler(SynchronizationContext context);
+
+        public EventHandler<FFMpegOutputEventArgs> ErrorDataReceived;
+
+        public delegate void ErrorDataReceivedEventHandler(SynchronizationContext context);
+
+        
+        public string FFMPEGPath { get; private set; }
+           
+
+        public Process FFMpegProcess { get; private set; }
+
+        public bool IsRunning { get; private set; }
+        public StreamWriter InputFFmpeg { get; private set; }
+
+        private CancellationTokenSource CancellationToken { get; set; }
+
+        public FFProcessWrapper(string ffmpegPath)
+        {
+            if (File.Exists(ffmpegPath))
+                FFMPEGPath = ffmpegPath;
+            else throw new
+                    FileNotFoundException("Can't localize ffmpeg.exe", ffmpegPath);
+        }
+
+
+        public async void Test()
+        {
+            await Start("-version");
+        }
+
+
+        public async Task Start(string arguments, CancellationTokenSource tokenSource = default)
+        {
+            if (FFMpegProcess != null)
+                throw new InvalidOperationException("The ffmpeg process is already launched");
+
+            if (tokenSource == default(CancellationTokenSource))
+                tokenSource = new CancellationTokenSource();
+
+            CancellationToken = tokenSource;        
+
+
+            FFMpegProcess = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = FFMPEGPath,
+                    Arguments = arguments,
+                    CreateNoWindow = true,
+                    UseShellExecute = false,
+                    WindowStyle = ProcessWindowStyle.Hidden,
+                    RedirectStandardOutput = true,
+                    RedirectStandardError = true,
+                    RedirectStandardInput = true,
+                    StandardErrorEncoding = Encoding.UTF8,
+                    StandardOutputEncoding = Encoding.UTF8
+                }
+            };      
+
+            try
+            {
+                FFMpegProcess.Start();
+                await Task.WhenAny(
+                  ReadStream(FFMpegProcess.StandardOutput, OnOutputReceived, CancellationToken.Token),
+                  ReadStream(FFMpegProcess.StandardError, OnErrorReceived, CancellationToken.Token),
+                  FFMpegProcess.WaitForExitAsync(CancellationToken.Token)
+                  );
+            }
+            finally
+            {
+                CancellationToken = null;
+                DisposingFFmpegProcess();
+            }
+        }
+
+
+        private void DisposingFFmpegProcess()
+        {
+            FFMpegProcess?.Dispose();
+            FFMpegProcess = null;
+        }
+
+
+        private Task OnErrorReceived(string obj)
+        {
+            var e = new FFMpegOutputEventArgs(obj);
+            return ErrorDataReceived.InvokeAsync(this, e);
+        }
+
+        private Task OnOutputReceived(string obj)
+        {
+            var e = new FFMpegOutputEventArgs(obj);
+            return OutputDataReceived.InvokeAsync(this, e);
+        }
+
+        private async static Task ReadStream(TextReader textReader, Func<string, Task> callback, CancellationToken token)
+        {
+            while (!token.IsCancellationRequested)
+            {           
+                var line = await textReader.ReadLineAsync();
+                if (line == null)
+                    break;
+
+                await callback(line);
+            }
+        }
+
+        public async Task Stop()
+        {
+            if (FFMpegProcess == null)
+                return;
+
+            //Try to exit ffmpeg with the command q
+            FFMpegProcess.StandardInput?.Write("q");
+
+            //Wait 2sec for the process to exited
+            await Task.WhenAny(Task.Delay(2000), Task.Run(async ()=>
+            {
+                while(!(FFMpegProcess?.HasExited ?? true))
+                {
+                    await Task.Delay(100);
+                }
+            }));
+
+            //If process is disposed do return
+            if (FFMpegProcess == null)
+                return;
+
+            //Try to cancel
+            CancellationToken.Cancel();
+            await Task.Delay(500);
+
+            //Kill the process if already present
+            if (FFMpegProcess != null && !FFMpegProcess.HasExited)
+                Kill();
+        }
+
+
+        public void Kill()
+        {
+            FFMpegProcess?.Kill();
+            DisposingFFmpegProcess();
+        }
+
+
+    }
+
+    public class FFMpegOutputEventArgs : DeferredEventArgs
+    {
+
+        public FFMpegOutputEventArgs(string data)
+        {
+            Data = data;
+        }
+        public string Data { get; }
+    }
+}
